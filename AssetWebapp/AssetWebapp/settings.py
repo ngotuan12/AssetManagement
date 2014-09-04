c7136516221db8ebf4b37211240f8c74798225ba
Ngo Anh Tuan <tuanna@email.ex-artisan.com>
2014-09-04 08:07:29 +0700

    update

--- AssetWebapp/AssetWebapp/settings.py
+++ AssetWebapp/AssetWebapp/settings.py
@@ -61,7 +61,7 @@ DATABASES = {
         'NAME': 'db',
         'USER': 'ASSET',
         'PASSWORD': 'asset',
-        'HOST': '10.0.2.102',
+        'HOST': '127.0.0.1',
         'PORT': '1521',
     }
 }
