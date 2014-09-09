f8854db7ec1461605b2fa0f4022e2747945f6502
Ngô Đắc Quang Vinh <quangvinh.itbk@gmail.com>
2014-09-09 23:18:42 +0700

    vinh update

--- AssetWebapp/myapp/views/AmortizeAsset.py
+++ AssetWebapp/myapp/views/AmortizeAsset.py
@@ -18,12 +18,15 @@ def index(request):
         context={}
         if(request.POST):
             month=request.POST['dtAmortizeMonth']
-            dMonth=datetime.strptime(month,'%dd/%mm/%yyyy')
             serial=request.POST['txtSerial']
             cursor=connection.cursor()
             p_error=''
             p_amount=cursor.var(cx_Oracle.NUMBER)
-            cursor.callproc("pck_asset.calc_amortize",(p_error,p_amount,serial.strip(),dMonth,request.user.username))
+            cursor.execute("ALTER SESSION SET NLS_DATE_FORMAT = 'DD/MM/YYYY HH24:MI:SS' "  
+                                       "NLS_TIMESTAMP_FORMAT = 'DD/MM/YYYY HH24:MI:SS.FF'")
+            cursor.callproc("pck_asset.calc_amortize",(p_error,p_amount,serial.strip(),month,request.user.username))
+            cursor.execute("ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD HH24:MI:SS' "  
+                                       "NLS_TIMESTAMP_FORMAT = 'YYYY-MM-DD HH24:MI:SS.FF'")
             cursor.close()
             if p_error.strip()!='':
                 context.update({"has_error":p_error})
