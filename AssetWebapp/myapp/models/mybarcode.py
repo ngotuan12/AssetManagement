09bca2f0c5c89e0998241a8a6de45fcf2f88bc45
diennd <ducdienpt@gmail.com>
2014-09-29 13:52:02 +0700

    d

--- AssetWebapp/myapp/models/mybarcode.py
+++ AssetWebapp/myapp/models/mybarcode.py
@@ -1,6 +1,6 @@
 #    mybarcode.py
 from reportlab.lib.units import mm
-from reportlab.graphics.barcode import createBarcodeDrawing
+from reportlab.graphics.barcode import createBarcodeDrawing,qr
 from reportlab.graphics.shapes import Drawing, String
 from reportlab.graphics.charts.barcharts import HorizontalBarChart
 
@@ -10,7 +10,11 @@ class MyBarcodeDrawing(Drawing):
         Drawing.__init__(self,barcode.width,barcode.height,*args,**kw)       
         self.add(barcode, name='barcode')
         
-
+class MyQRcodeDrawing(Drawing):
+    def __init__(self, text_value, *args, **kw):
+        qr_code = qr.QrCodeWidget( text_value )
+        Drawing.__init__(self, width=100, height=100)
+        self.add(qr_code, name='qr_code')
 if __name__=='__main__':
     #use the standard 'save' method to save barcode.gif, barcode.pdf etc
     #for quick feedback while working.
